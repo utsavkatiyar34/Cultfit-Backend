fe3824deac372bc1e776bd6045c60b79834423cd
Priyam <priyamktr7@gmail.com>
2022-09-05 12:42:29 +0530

    database hosted on cloud

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDb() {
     return new Promise((resolve, reject) => {
-        const url = "mongodb://127.0.0.1:27017/cultfit";
+        const url = "mongodb+srv://utsavkatiyar34:Utsav123%4047@cluster0.ukyjn.mongodb.net/cultfit?retryWrites=true&w=majority";
         mongoose.connect(url, (err) => {
             if (err) {
                 return reject(err)
@@ -15,3 +15,29 @@ async function connectDb() {
 }
 
 module.exports = connectDb;
+
+
+
+
+
+
+
+// const mongoose = require("mongoose");
+
+// async function connectDb() {
+//     return new Promise((resolve, reject) => {
+//         const url = "mongodb+srv://utsavkatiyar34:Utsav123%4047@cluster0.ukyjn.mongodb.net/cultfit?retryWrites=true&w=majority";
+//         mongoose.connect(url, {
+//             useNewUrlParser:true,
+//             useUnifiedTopology:true
+//         }).then(()=>{
+//         console.log("Successfully connected to database");
+        
+//         }).catch((err)=>{
+//           console.log(err);  
+
+//         })
+//     })
+// }
+
+// module.exports = connectDb;
